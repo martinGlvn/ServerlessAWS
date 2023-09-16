2d5438dce2678a77ed47d553108e630c8b0044f6
martinGlvn <lkn.galvan.martin@gmail.com>
2023-09-15 21:49:25 -0300

    update

--- taskApi/src/handlers/updateTask.js
+++ taskApi/src/handlers/updateTask.js
@@ -1,6 +1,7 @@
 module.exports.handler = (event, context, callback) => {
+  const id = event.pathParameters.id;
   callback(null, {
     statusCode: 200,
-    body: JSON.stringify({ msg: "Task Update" }),
+    body: JSON.stringify({ msg: `Task ${id} Update` }),
   });
 };
