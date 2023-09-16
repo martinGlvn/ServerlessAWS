985e34257ed0ccb5e9f08d388f15c86de1907115
martinGlvn <lkn.galvan.martin@gmail.com>
2023-09-16 20:25:11 -0300

    createTask

--- taskApi/src/handlers/createTask.js
+++ taskApi/src/handlers/createTask.js
@@ -1,5 +1,25 @@
-module.exports.handler = (event, context, callback) => {
+const documentClient = require("../utils/database");
+const TASKS_TABLE_NAME = process.env.TASKS_TABLE_NAME;
+
+//
+module.exports.handler = async (event, context, callback) => {
   const data = JSON.parse(event.body);
+  try {
+    const params = {
+      TableName: TASKS_TABLE_NAME,
+      Item: {
+        id: data.id,
+        title: data.title,
+        desc: data.desc,
+      },
+      ConditionExpression: "attribute_not_exists(id)",
+    };
+    await documentClient.put(params).promise();
+    callback(null, response.send(201, data));
+  } catch (error) {
+    console.error(JSON.stringify(error));
+    callback(null, response.send(500, { err: error.message }));
+  }
   callback(null, {
     statusCode: 201,
     body: JSON.stringify({ msg: "Task C reate", data: data }),
